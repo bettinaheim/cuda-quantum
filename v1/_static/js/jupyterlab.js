baca75739de074c7e36abbfe09cf7272cdc381db
Bettina Heim <heimb@outlook.com>
2023-11-07 10:30:55 +0100

    Update jupyterlab.js

--- v1/_static/js/jupyterlab.js
+++ v1/_static/js/jupyterlab.js
@@ -1,6 +1,7 @@
-var launchJupyterLab = (frame_id, notebook_path) => {
+var launchJupyterLab = (frame_id, notebook_path = null) => {
     let token = prompt("Enter Token:");
-    let url="http://localhost:5801/lab?token="
+    let url="http://localhost:5801/doc/"
     var frame = document.getElementById(frame_id);
-    frame.src = url + token;
+    var path = notebook_path == null ? "" : "tree/" + notebook_path;
+    frame.src = url + path + "?token=" + token;
 }
