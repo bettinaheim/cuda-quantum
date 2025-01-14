f27369aca790daec5f7703926275a837fa5844e3
A.M. Santana <anthonys@nvidia.com>
2025-01-14 18:26:57 +0000

    push full implementation of manipulation.py in cpp
    Signed-off-by: A.M. Santana <anthonys@nvidia.com>

--- runtime/cudaq/dynamics/helpers.cpp
+++ runtime/cudaq/dynamics/helpers.cpp
@@ -14,12 +14,40 @@ class _OperatorHelpers {
 public:
   _OperatorHelpers() = default;
 
+  /// Generates all possible states for the given dimensions ordered according
+  /// to the sequence of degrees (ordering is relevant if dimensions differ).
+  static std::vector<std::string>
+  generate_all_states(std::vector<int> degrees, std::map<int, int> dimensions) {
+    if (degrees.size() == 0)
+      return {};
+
+    std::vector<std::string> states;
+    int range = dimensions[degrees[0]];
+    for (auto state = 0; state < range; state++) {
+      states.push_back(std::to_string(state));
+    }
+
+    for (auto degree = degrees.begin() + 1; degree != degrees.end(); ++degree) {
+      std::string term;
+      std::vector<std::string> result;
+      for (auto current : states) {
+        for (auto state = 0; state < dimensions[degrees[*degree]]; state++) {
+          result.push_back(current + std::to_string(state));
+        }
+      }
+      states = result;
+    }
+
+    return states;
+  }
+
   // Permutes the given matrix according to the given permutation.
   // If states is the current order of vector entries on which the given matrix
   // acts, and permuted_states is the desired order of an array on which the
   // permuted matrix should act, then the permutation is defined such that
   // [states[i] for i in permutation] produces permuted_states.
-  cudaq::matrix_2 permute_matrix(cudaq::matrix_2 matrix, std::vector<std::size_t> permutation) {
+  static cudaq::matrix_2 permute_matrix(cudaq::matrix_2 matrix,
+                                        std::vector<int> permutation) {
     auto result = cudaq::matrix_2(matrix.get_rows(), matrix.get_columns());
     std::vector<std::complex<double>> sorted_values;
     for (std::size_t permuted : permutation) {
@@ -28,13 +56,18 @@ public:
       }
     }
     int idx = 0;
-    for (std::size_t row=0; row < result.get_rows(); row++) {
-      for (std::size_t col=0; col < result.get_columns(); col++) {
-        result[{row,col}] = sorted_values[idx];
+    for (std::size_t row = 0; row < result.get_rows(); row++) {
+      for (std::size_t col = 0; col < result.get_columns(); col++) {
+        result[{row, col}] = sorted_values[idx];
         idx++;
       }
     }
     return result;
   }
 
+  // Returns the degrees sorted in canonical order.
+  static std::vector<int> canonicalize_degrees(std::vector<int> degrees) {
+    std::sort(degrees.begin(), degrees.end(), std::greater<int>());
+    return degrees;
+  }
 };
--- runtime/cudaq/manipulation.h
+++ runtime/cudaq/manipulation.h
@@ -13,16 +13,16 @@
 
 namespace cudaq {
 
-template<typename TEval>
+template <typename TEval>
 class OperatorArithmetics {
 public:
   /// @brief Accesses the relevant data to evaluate an operator expression
   /// in the leaf nodes, that is in elementary and scalar operators.
   TEval evaluate(std::variant<elementary_operator, scalar_operator> op);
-  
+
   /// @brief Adds two operators that act on the same degrees of freedom.
   TEval add(TEval val1, TEval val2);
-  
+
   /// @brief Multiplies two operators that act on the same degrees of freedom.
   TEval mul(TEval val1, TEval val2);
 
@@ -32,42 +32,55 @@ public:
 };
 
 class EvaluatedMatrix {
+  friend class MatrixArithmetics;
+
 private:
   std::vector<int> m_degrees;
   matrix_2 m_matrix;
 
 public:
-  EvaluatedMatrix(std::vector<int> degrees, matrix_2 matrix) : m_degrees(degrees), m_matrix(matrix) {}
-  
+  EvaluatedMatrix(std::vector<int> degrees, matrix_2 matrix)
+      : m_degrees(degrees), m_matrix(matrix) {}
+
   /// @brief The degrees of freedom that the matrix of the evaluated value
   /// applies to.
   std::vector<int> degrees() { return m_degrees; }
-  
+
   /// @brief The matrix representation of an evaluated operator, according
   /// to the sequence of degrees of freedom associated with the evaluated
   /// value.
   matrix_2 matrix() { return m_matrix; }
 };
 
-
 /// Encapsulates the functions needed to compute the matrix representation
 /// of an operator expression.
 class MatrixArithmetics : public OperatorArithmetics<EvaluatedMatrix> {
 private:
-  std::map<int,int> m_dimensions;
+  std::map<int, int> m_dimensions;
   std::map<std::string, std::complex<double>> m_parameters;
-  void _compute_permutation(std::vector<int> op_degrees, std::vector<int> canon_degrees);
-  void _canonicalize(matrix_2 op_matrix, std::vector<int> op_degrees);
-  
+  std::vector<int> _compute_permutation(std::vector<int> op_degrees,
+                                        std::vector<int> canon_degrees);
+  std::tuple<matrix_2, std::vector<int>>
+  _canonicalize(matrix_2 &op_matrix, std::vector<int> op_degrees);
+
 public:
-  MatrixArithmetics(std::map<int,int> dimensions, std::map<std::string, std::complex<double>> parameters) : m_dimensions(dimensions), m_parameters(parameters) {}
+  MatrixArithmetics(std::map<int, int> dimensions,
+                    std::map<std::string, std::complex<double>> parameters)
+      : m_dimensions(dimensions), m_parameters(parameters) {}
 
+  // Computes the tensor product of two evaluate operators that act on
+  // different degres of freedom using the kronecker product.
   EvaluatedMatrix tensor(EvaluatedMatrix op1, EvaluatedMatrix op2);
+  // Multiplies two evaluated operators that act on the same degrees
+  // of freedom.
   EvaluatedMatrix mul(EvaluatedMatrix op1, EvaluatedMatrix op2);
+  // Adds two evaluated operators that act on the same degrees
+  // of freedom.
   EvaluatedMatrix add(EvaluatedMatrix op1, EvaluatedMatrix op2);
-  EvaluatedMatrix evaluate(std::variant<elementary_operator, scalar_operator> op);
+  // Computes the matrix of an ElementaryOperator or ScalarOperator using its
+  // `to_matrix` method.
+  EvaluatedMatrix
+  evaluate(std::variant<elementary_operator, scalar_operator> op);
 };
-
-
 
 } // namespace cudaq
